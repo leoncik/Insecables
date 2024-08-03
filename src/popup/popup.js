5fc5aabfd8a3d01c7965d46fb2d2021e92d8a73f
Léonard Wojcik <leonardw@tutanota.com>
2024-08-03 18:49:21 +0200

    feat: add spaces copy

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,1 +1,13 @@
-console.log("JS loaded!")
+const copyButtons = document.querySelectorAll(".copyButton");
+
+copyButtons.forEach((copyButton) => {
+  copyButton.addEventListener("click", () => {
+    const textToCopy =
+      copyButton.previousElementSibling.getAttribute("data-text-value");
+    copyToClipboard(textToCopy);
+  });
+});
+
+async function copyToClipboard(text) {
+  await navigator.clipboard.writeText(text);
+}
