c9129e81465c1ba60dfbd9a2cdc887dc8a7c648d
Léonard Wojcik <leonardw@tutanota.com>
2024-08-26 20:30:21 +0200

    feat: closing popup on copy

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -5,6 +5,7 @@ copyButtons.forEach((copyButton) => {
   copyButton.addEventListener("click", () => {
     const textToCopy = copyButton.closest('.element').querySelector('p').getAttribute("data-text-value");
     copyToClipboard(textToCopy);
+    window.close()
   });
 });
 
