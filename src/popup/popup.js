e8a1b5debed4a49b3e4ad54be443ea0f2b71c30d
Léonard Wojcik <leonardw@tutanota.com>
2024-10-01 19:50:34 +0200

    fix: copy asynchronously
    The windows was closing before the content was copied.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,20 +2,31 @@ const copyButtons = document.querySelectorAll(".copyButton");
 const copyHtmlButtons = document.querySelectorAll(".copyHtmlButton");
 
 copyButtons.forEach((copyButton) => {
-  copyButton.addEventListener("click", () => {
-    const textToCopy = copyButton.closest('.element').querySelector('p').getAttribute("data-text-value");
-    copyToClipboard(textToCopy);
-    window.close()
+  copyButton.addEventListener("click", async () => {
+    const textToCopy = copyButton
+      .closest(".element")
+      .querySelector("p")
+      .getAttribute("data-text-value");
+    await copyToClipboard(textToCopy);
+    window.close();
   });
 });
 
 copyHtmlButtons.forEach((copyHtmlButton) => {
-  copyHtmlButton.addEventListener("click", () => {
-    const htmlToCopy = copyHtmlButton.closest('.element').querySelector('p').getAttribute("data-html-value");
-    copyToClipboard(htmlToCopy);
+  copyHtmlButton.addEventListener("click", async () => {
+    const htmlToCopy = copyHtmlButton
+      .closest(".element")
+      .querySelector("p")
+      .getAttribute("data-html-value");
+    await copyToClipboard(htmlToCopy);
   });
 });
 
 async function copyToClipboard(text) {
-  await navigator.clipboard.writeText(text);
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log("Content copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
 }
